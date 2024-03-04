3c85cc5fcc8d9c227576e6b2d65e639e82431d5c
ahmed hany <ahany963963@gmail.com>
2024-03-04 21:06:15 +0200

    titans + wall + lanes

---
+++ src/game/engine/lanes/Lane.java
@@ -0,0 +1,42 @@
+package game.engine.lanes;
+import game.engine.base.*;
+import game.engine.Weapons.*;
+import game.engine.titans.*;
+import java.util.*;
+public class Lane implements Comparable{
+	private final Wall laneWall;
+	private int dangerLevel;
+	private final PriorityQueue<Titan> titans;
+	private final ArrayList<Weapon> weapons;
+	public Lane(Wall laneWall) {
+		this.laneWall = laneWall;
+		this.dangerLevel = 0;
+		this.titans = null;
+		this.weapons = null;
+	}
+	
+	public int getDangerLevel() {
+		return dangerLevel;
+	}
+
+	public void setDangerLevel(int dangerLevel) {
+		this.dangerLevel = dangerLevel;
+	}
+
+	public Wall getLaneWall() {
+		return laneWall;
+	}
+
+	public PriorityQueue<Titan> getTitans() {
+		return titans;
+	}
+
+	public ArrayList<Weapon> getWeapons() {
+		return weapons;
+	}
+
+	public int compareTo(Lane o) {
+		return this.dangerLevel - ((Lane)o).dangerLevel;
+	}
+}
+
