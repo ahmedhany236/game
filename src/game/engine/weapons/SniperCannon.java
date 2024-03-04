13d625446d705d64bbbc0a82608cba68179de639
Mohamed Abdelghany <mmabdelghany2004@gmail.com>
2024-03-04 21:04:36 +0200

    added interfaces + weapon class

---
+++ src/game/engine/weapons/SniperCannon.java
@@ -0,0 +1,8 @@
+package game.engine.weapons;
+
+public class SniperCannon extends Weapon{
+	public static final int WEAPON_CODE = 2;
+	public SniperCannon (int baseDamage) {
+		super (baseDamage);
+	}
+}
